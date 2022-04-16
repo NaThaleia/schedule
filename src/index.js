4644d68e13dd7fbe5a3b4d90c4f8ae8e37b0436c
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2022-04-16 04:05:58 +0200

    authorization

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import './sass/main.scss';
 import { refs } from './js/base/refs';
 import login from './templates/login.hbs';
+import loginPas from './api/login.json';
 
 const start = () => {
-  refs.conteiner.insertAdjacentHTML('beforeend', login());
+  refs.login.insertAdjacentHTML('beforeend', login());
+  authorization();
 };
 
 start();
+
+function authorization() {
+  const refAuthorization = document.querySelector('.js-authorization');
+  refAuthorization.addEventListener('submit', onform);
+}
+
+function onform(e) {
+  e.preventDefault();
+  const login = e.target[0].value;
+  const password = e.target[1].value;
+
+  const promise = new Promise(resolve => {
+    resolve(loginPas);
+  });
+
+  promise.then(data => {
+    const user = data.find(e => e.login === login);
+
+    if (user.password === password) {
+      console.log('вы авторизированны');
+    } else {
+      console.log('вы ввели неправильный логин или пароль');
+      return;
+    }
+    return data;
+  });
+}
--- src/js/base/refs.js
+++ src/js/base/refs.js
@@ -1,3 +1,5 @@
 export const refs = {
-  conteiner: document.querySelector('.js-container'),
+  login: document.querySelector('.js-login'),
+  heder: document.querySelector('.js-heder'),
+  container: document.querySelector('.js-container'),
 };
