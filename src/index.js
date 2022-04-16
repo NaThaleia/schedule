a88e6cd10d47462ee1ef77bfd1650585098c8a35
Дмитрий Горелкин <87872078+Dmitry-Gorelkin@users.noreply.github.com>
2022-04-16 02:37:43 +0200

    var_one_block

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss';
+import { refs } from './js/base/refs';
+import login from './templates/login.hbs';
+
+const start = () => {
+  refs.conteiner.insertAdjacentHTML('beforeend', login());
+};
+
+start();
